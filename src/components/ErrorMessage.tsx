a84f079fb231c8907c38035759fb6a7124dfd8da
vinithecsar <viniciuscesarbrito@gmail.com>
2023-12-18 13:11:12 -0300

    ajuste no tamanho do erro

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,7 +1,9 @@
 export default function ErrorComponent(props: { error: string }) {
   return (
-    <div className="fixed top-1/4 m-auto flex h-[6%] w-[15%] flex-col items-center justify-center rounded-lg bg-red-500">
-      <h1 className="font-extrabold text-white">{props.error}</h1>
+    <div className="fixed top-1/4 m-auto flex h-[10%] w-[25%] flex-col items-center justify-center rounded-lg bg-red-500">
+      <h1 className="m-auto font-extrabold text-white xl:text-lg">
+        {props.error}
+      </h1>
     </div>
   );
 }
